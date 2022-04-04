43b2c9061aa761977c7eaf5ec6a3db750ff2cf5e
pushvish111 <pushvish111@gmail.com>
2022-04-04 16:15:00 +0530

    code but not tested

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PushNotification from 'react-native-push-notification';
 
 export default function App() {
+  componentDidMount()
+  console.log('Did mount');
+
+  PushNotification.configure({
+    onRegister: function (token) {
+      console.log('TOKEN:', token);
+      },
+    onNotification: function (notification) {
+      console.log('NOTIFICATION:', notification);
+      },
+
+      //this id is of the firebase user id
+      senderID: '719007178572',
+
+      popInitialNotification: true,
+      requestPermissions: true,
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
--- android/app/src/main/java/com/pushsnstest/MainApplication.java
+++ android/app/src/main/java/com/pushsnstest/MainApplication.java
@@ -20,6 +20,8 @@ import com.facebook.react.bridge.JSIModulePackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
     this,
@@ -35,7 +37,10 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
-      return packages;
+       return Arrays.<ReactPackage>asList(
+              new MainReactPackage(),
+              new ReactNativePushNotificationPackage() // <---- Add the Package
+        );
     }
 
     @Override
